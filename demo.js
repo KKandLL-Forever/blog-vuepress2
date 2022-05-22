ee6b19ec558696db6ac26080e8a326d3fcf15fd1
KKandLL-Forever <clay1wang@gmail.com>
2022-05-22 12:17:17 +0800

    feat: article post
    article post

---
+++ demo.js
@@ -0,0 +1,12 @@
+let list = [1,2]
+
+let proxyList = new Proxy(list,{
+  set(target,property,value){
+    console.log(property,'property')
+    console.log(value,'value')
+    target[property] = value
+    return true
+  }
+})
+proxyList.pop()
+console.log(proxyList,'list')
